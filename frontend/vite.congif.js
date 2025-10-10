7e99a78187e31266b2a8226c0e5c4b6cf4d2b38e
mcdonan-sys <mcdonan@grace.edu>
2025-10-09 20:29:41 -0400

    Update vite.congif.js

--- frontend/vite.congif.js
+++ frontend/vite.congif.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
 
 export default defineConfig({
   plugins: [react()],
